abec72f766ae5727fb8e4d3ee9dc0cd5179fb6d4
Emilio <emilio.pg95@gmail.com>
2025-01-17 20:46:20 -0600

    Refactor controller and model for improved code clarity and consistency

--- model/todo.js
+++ model/todo.js
@@ -1,18 +1,12 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable arrow-body-style */
-/* eslint-disable linebreak-style */
 const pool = require('./database');
 
-const create = (description) => {
-  return pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [description]);
-};
+const create = (description) =>
+  pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [
+    description,
+  ]);
 
-const get = () => {
-  return pool.query('SELECT * FROM todo');
-};
+const get = () => pool.query('SELECT * FROM todo');
 
-const remove = (id) => {
-  return pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
-};
+const remove = (id) => pool.query('DELETE FROM todo WHERE todo_id = $1', [id]);
 
 module.exports = { create, get, remove };
